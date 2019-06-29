bb5101a9cdfbc2a3b4508fb987736e159339d3de
ericsonsilva <31900194+ericsonsilva@users.noreply.github.com>
2019-06-29 14:45:50 -0300

    tabelas

--- main.lua
+++ main.lua
@@ -1,1 +1,10 @@
+messege=0
+-- tabelas
+testscore={95, 87, 98}
 
+messege=testscore[1]
+
+function love.draw()
+  love.graphics.setFont(love.graphics.newFont(50))
+  love.graphics.print(messege)
+end
