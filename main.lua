d283d8c576c7528b39f5f167e39f6ac5fd805239
ericsonsilva <31900194+ericsonsilva@users.noreply.github.com>
2019-06-29 15:13:38 -0300

    tratamentos de tabelas

--- main.lua
+++ main.lua
@@ -2,6 +2,8 @@ messege=0
 -- tabelas
 --testscore={95, 87, 98}
 
+--inserindo valor na tabela
+--[[
 testscore={}
 
 table.insert(testscore, 95)
@@ -9,8 +11,18 @@ table.insert(testscore, 87)
 table.insert(testscore, 98)
 
 messege=testscore[3]
+]]
+
+-- função de manuipulação de itens na tabela
+testscore={95, 87, 98}
+
+testscore.subject = "history"
+
+for i,s in ipairs(testscore) do
+  messege = messege + i
+end
 
 function love.draw()
   love.graphics.setFont(love.graphics.newFont(50))
-  love.graphics.print(messege)
+  love.graphics.print(testscore.subject)
 end
