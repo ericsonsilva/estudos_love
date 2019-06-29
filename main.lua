bae450f41cd0772dc547e4d601ecad9f5924f8d0
ericsonsilva <31900194+ericsonsilva@users.noreply.github.com>
2019-06-29 14:53:28 -0300

    table.isert(tablename, value)

--- main.lua
+++ main.lua
@@ -1,8 +1,14 @@
 messege=0
 -- tabelas
-testscore={95, 87, 98}
+--testscore={95, 87, 98}
 
-messege=testscore[1]
+testscore={}
+
+table.insert(testscore, 95)
+table.insert(testscore, 87)
+table.insert(testscore, 98)
+
+messege=testscore[3]
 
 function love.draw()
   love.graphics.setFont(love.graphics.newFont(50))
